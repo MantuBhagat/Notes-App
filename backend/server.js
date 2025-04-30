93e21c29fd1881f4b252fc9b198ee4e8aa543313
Mantu Bhai <quantumcoderrr@gmail.com>
2025-04-30 15:56:43 +0530

    Folder Add

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hey");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
